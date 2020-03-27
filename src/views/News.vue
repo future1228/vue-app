729690041bafec3cd9f9d058300bbb68b9215a52
Future <eskimo1228@outlook.com>
2020-03-27 23:46:28 +0300

    create News component

--- src/views/News.vue
+++ src/views/News.vue
@@ -1,5 +1,47 @@
 <template>
   <div class="about">
     <h1>Sports News</h1>
+    <table border="1">
+        <thead>
+            <tr>
+                <th>Entry</th>
+                <th>Entry</th>
+                <th>Entry</th>
+                <th>Entry</th>
+                <th>Entry</th>
+                <th>Entry</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Sample</td>
+                <td>Filter</td>
+                <td>12-11-2011 11:11</td>
+                <td>OK</td>
+                <td>123</td>
+                <td>Do some other</td>
+            </tr>
+            <tr>
+                <td>Try</td>
+                <td>It</td>
+                <td>11-20-2013 08:56</td>
+                <td>It</td>
+                <td>Works</td>
+                <td>Do some FILTERME</td>
+            </tr>
+            <tr>
+                <td>§</td>
+                <td>$</td>
+                <td>%</td>
+                <td>&</td>
+                <td>/</td>
+                <td>!</td>
+            </tr>
+        </tbody>
+    </table>   
   </div>
 </template>
+
+<style>
+
+</style>
